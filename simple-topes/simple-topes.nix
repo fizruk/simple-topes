f9dc9ae06a4f9eff2bae29b35bbaf5b513266d32
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2022-06-18 15:10:58 +0300

    Do not use hpack with Nix

--- simple-topes/simple-topes.nix
+++ simple-topes/simple-topes.nix
@@ -1,5 +1,5 @@
 { mkDerivation, array, base, Cabal, cabal-doctest, doctest, Glob
-, hpack, lib, logict, mtl, QuickCheck, template-haskell
+, lib, logict, mtl, QuickCheck, template-haskell
 }:
 mkDerivation {
   pname = "simple-topes";
@@ -9,12 +9,10 @@ mkDerivation {
   isExecutable = true;
   setupHaskellDepends = [ base Cabal cabal-doctest ];
   libraryHaskellDepends = [ array base logict mtl ];
-  libraryToolDepends = [ hpack ];
   executableHaskellDepends = [ array base logict mtl ];
   testHaskellDepends = [
     array base doctest Glob logict mtl QuickCheck template-haskell
   ];
-  prePatch = "hpack";
   homepage = "https://github.com/githubuser/simple-topes#readme";
   license = lib.licenses.bsd3;
 }
